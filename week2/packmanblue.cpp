a003cb96d7ebd58da7b292ab0e859ea025cac23f
mahnoorriaz-jpg <mahnoorriaz00002@gmail.com>
2026-02-23 23:51:34 +0500

    Added pacmanblue.cpp file

---
+++ week2/packmanblue.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ using namespace std;
+int main()
+ {
+ system("color 37");    
+
+cout<<"             .::---::...        "<< endl;
+cout<<"          .--------------.      "<< endl;
+cout<<"        .------------------.    "<< endl;
+cout<<"        -----------------:.     "<< endl;
+cout<<"      :-------------::.         "<< endl;
+cout<<"      -------------:.           "<< endl;
+cout<<"      :---------------:..       "<< endl;
+cout<<"       :------------------:.    "<< endl;
+cout<<"         .----------------:     "<< endl;
+cout<<"            .:---------:.       "<< endl;
+    return 0;
+}
