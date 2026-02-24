e889137e9a2d407d09baf118baaf859a148a0f85
mahnoorriaz-jpg <mahnoorriaz00002@gmail.com>
2026-02-24 23:18:43 +0500

    added task8.cpp file

---
+++ week2/task8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+main()
+{
+ system("color 57");
+
+ cout<< "    ...            ooooo     ooooo          ..........       "<<endl;   
+ cout<< "    ....          o     o   o     o         .........     "<<endl;  
+ cout<< "    .....            ooo    o     o         .......      "<<endl;              
+ cout<< "    ......         oo       o     o         ......        "<<endl; 
+ cout<< "    .......        oooooo    ooooo  th      .....             "<<endl;
+ cout<< "     .......      ......................    ....           "<<endl;
+ cout<< "       ......       c e n t u r y           ...             "<<endl;
+ cout<< "         .....    ......................    ..                "<<endl;
+ cout<< "           ...    oooo oooo o  o  oooo      .          "<<endl;  
+ cout<< "          _||_     oo  o     oo    oo      _||              _"<<endl;    
+ cout<< "         |    |    OO  oooo  oo    oo     |    |          "<<endl;       
+ cout<< "     ____|____|___ oo  oooo o  o   oo  ___|____|___      "<<endl;
+
+}
