ad08cb7499b3aec368555c0557b2bbac3e172019
mahnoorriaz-jpg <mahnoorriaz00002@gmail.com>
2026-02-24 23:47:53 +0500

    added task9.cpp file

---
+++ week2/task9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+main()
+{
+ system("color 87");
+
+ cout <<"              -----------------------------------------------"<<endl;  
+ cout <<"                          ^ _ ^                               "<<endl;  
+ cout <<"                         (o o)\\_________                     "<<endl;    
+ cout <<"                                                             "<<endl;           
+ cout <<"                         (___)\\         )\\/\\                 "<<endl;
+ cout <<"                              | |-----w  |                    "<<endl;
+ cout <<"                              | |      | |                    "<<endl; 
+
+        
+}
