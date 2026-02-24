3b031fa7bf87d34430d049b31c3f69fdcc9f3140
mahnoorriaz-jpg <mahnoorriaz00002@gmail.com>
2026-02-25 00:09:01 +0500

    added starwar.cpp file

---
+++ week2/starwar.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{  
+  system("color 07");
+ cout <<"                    ooooooo  oooooo ooooooo   oooooo     oo  o   ooo   ooooooo  ooooo  ooooo"<<endl;
+ cout <<"                    oo         oo   o     o   o    o     oo  oo   oo   o     o  o   o  oo   "<<endl;
+ cout <<"                      oooo     oo   ooooooo   oooo       oo  oo   o    ooooooo  oooo     ooo"<<endl;
+ cout <<"                        oo     oo   o     o   o   oo      oo oo  oo    o     o  o   oo    oo"<<endl;
+ cout <<"                    oooooo     oo   o     o   o    oo     ooo  ooo     o     o  o   ooo oooo"<<endl;
+
+}
