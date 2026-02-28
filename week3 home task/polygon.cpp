89d80ab850f560af5b28ca2abe3b87e9d9df8195
mahnoorriaz-jpg <mahnoorriaz00002@gmail.com>
2026-03-01 01:02:15 +0500

    added week3 file

---
+++ week3 home task/polygon.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main() {
+int n;
+cout<<" Enter the numbers of sides: ";
+cin>>n;
+int sum;
+sum=(n-2)*180;
+cout<< "sum of internal angles : " <<sum<< " degrees " ;
+
+}
---
+++ week3 home task/vedioapplication.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main() {
+int m, n;
+cout<<" Numbers of Mintues: ";
+cin>>m;
+cout<<" Frames per second: ";
+cin>>n;
+int total;
+total=(m*60)*n ;
+cout<< "Total Numbers of Frames: " <<total;
+}
